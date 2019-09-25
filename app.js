30d1bca30b8dba6d2739b68767d2ba9f8347d338
Kenneth Lee <kyung.kenneth.lee@gmail.com>
2019-09-25 16:56:04 -0700

    implement the attack function

--- app.js
+++ app.js
@@ -10,6 +10,59 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack() {
+            const playerMax = 10;
+            const playerMin = 1;
+            const damage = this.generateDamage(playerMax, playerMin);
+
+            this.monsterHealth -= damage;
+
+            if(this.checkWin()) {
+                //no reason to continue so return
+                return;
+            }
+
+            const monsterMax = 12;
+            const monsterMin = 3
+
+            const monsterDamage = this.generateDamage(monsterMax, monsterMin);
+            this.playerHealth -= monsterDamage;
+
+            //damage to the monster
+            this.checkWin();
+
+
+        },
+        specialAttack() {
+
+        },
+        heal() {
+
+        },
+        giveUp() {
+
+        },
+        generateDamage(max, min) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+
+        checkWin() {
+            if(this.playerHealth <= 0) {
+                this.checkMessage('You lost! New Game?');
+                return true;
+            } else if(this.monsterHealth <= 0){
+                this.checkMessage('You win! New Game?');
+                return true;
+            }
+            return false;
+        },
+        checkMessage(message) {
+            if(confirm(message)) {
+                this.startGame()
+            } else {
+                this.gameIsRunning = false;
+            }
         }
     }
 })
